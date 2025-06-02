629cbffc9cdbf0ff4cab7ea7698d5ac06c64d807
pericabartulovic <pericab35@gmail.com>
2025-06-02 18:01:43 +0200

    dev/prod errors

--- controllers/errorController.mjs
+++ controllers/errorController.mjs
@@ -1,12 +1,52 @@
+import AppError from "../utils/appError.mjs";
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+}
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    })
+  }
+}
+
 const globalErrorHandler = (err, req, res, next) => {
   // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };  //to avoid alternating original object
+
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
 
 export default globalErrorHandler;
--- utils/catchAsync.mjs
+++ utils/catchAsync.mjs
@@ -1,5 +1,4 @@
 export default function catchAsync(fn) {
-  console.log(fn);
   return (req, res, next) => {
     fn(req, res, next).catch(next);
   };

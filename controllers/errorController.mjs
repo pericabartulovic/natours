f6d79e292dd70ee2cfba2d979f1c5fe3f5225830
pericabartulovic <pericab35@gmail.com>
2025-06-03 10:04:16 +0200

    prod errs further customization

--- controllers/errorController.mjs
+++ controllers/errorController.mjs
@@ -5,6 +5,11 @@ const handleCastErrorDB = err => {
   return new AppError(message, 400);
 }
 
+const handleDuplicateFieldsDB = err => {
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
+  return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -45,6 +50,8 @@ const globalErrorHandler = (err, req, res, next) => {
     let error = { ...err };  //to avoid alternating original object
 
     if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+
     sendErrorProd(error, res);
   }
 };

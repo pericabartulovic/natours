2d52b5365a92a81485c1dd9381af96995a4f5ed0
pericabartulovic <pericab35@gmail.com>
2025-06-05 00:41:44 +0200

    refference errors, unhandledRejection, uncaughtException

--- server.mjs
+++ server.mjs
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 import 'dotenv/config';
-import app from './app.mjs';
+// import app from './app.mjs';
+
+process.on('uncaughtException', err => {
+  console.log("UNCAUGHT EXEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
@@ -17,6 +23,27 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+let server;
+try {
+  const { default: app } = await import('./app.mjs');
+
+  server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+  });
+} catch (err) {
+  console.log('🔥 Startup failed!');
+  console.error(err);
+  process.exit(1);
+}
+// const server = app.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
+
+
+process.on("unhandledRejection", err => {
+  console.log("UNHANDLER REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });

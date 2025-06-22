b1181d651ba6c7c1289fb42b7553f7a25d90cc03
pericabartulovic <pericab35@gmail.com>
2025-06-23 01:30:40 +0200

    update non essential user data

--- controllers/userController.mjs
+++ controllers/userController.mjs
@@ -1,5 +1,14 @@
 import User from '../models/userModel.mjs';
+import AppError from '../utils/appError.mjs';
 import catchAsync from '../utils/catchAsync.mjs';
+
+const filterOjb = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+}
 
 const userController = {
   getAllUsers: catchAsync(async (req, res, next) => {
@@ -11,6 +20,29 @@ const userController = {
       data: {
         users,
       },
+    });
+  }),
+
+  updateMe: catchAsync(async (req, res, next) => {
+    // 1) Create error if user POSTs password data
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(new AppError('This route is not for password updates. Please use /updateMyPassword', 400));
+    }
+
+    // 2) Filter out unwanted fields name that are not allowed to be updated
+    const filteredBody = filterOjb(req.body, 'name', 'email');
+
+    // 3) Update user document
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser,
+      }
     });
   }),
 
--- models/userModel.mjs
+++ models/userModel.mjs
@@ -86,7 +86,5 @@ userSchema.methods.createPasswordResetToken = function () {
   return resetToken;
 }
 
-
 const User = mongoose.model('User', userSchema);
-
 export default User;

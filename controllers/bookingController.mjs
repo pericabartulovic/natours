364d4e87017a9b5b5aa9c86d813b386a07292549
pericabartulovic <pericab35@gmail.com>
2025-09-21 01:14:03 +0200

    WIP setting up credit card payment with stripe

--- controllers/bookingController.mjs
+++ controllers/bookingController.mjs
@@ -9,9 +9,9 @@ const bookingController = {
   createCheckoutSession: catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.tourId);
 
-    // if (!tour) {
-    //   return next(new AppError('There is no tour with that ID', 404));
-    // }
+    if (!tour) {
+      return next(new AppError('There is no tour with that ID', 404));
+    }
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
